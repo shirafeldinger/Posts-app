e3497ab846d7f5e5d7b9d8cb9ae90c450c039254
Shira Feldinger <shirafeldinger@gmail.com>
2020-07-21 13:57:33 +0300

    Updates

---
+++ precache-manifest.987dca011d78d5e6469fa67b5ea01124.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfb267924ddbda48a8529ac8c83d8221",
+    "url": "/Posts-app/index.html"
+  },
+  {
+    "revision": "a918006c8fba3a62c5c1",
+    "url": "/Posts-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "05b9e50729ffc18b8388",
+    "url": "/Posts-app/static/css/main.ef4f25c6.chunk.css"
+  },
+  {
+    "revision": "a918006c8fba3a62c5c1",
+    "url": "/Posts-app/static/js/2.87ad102a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Posts-app/static/js/2.87ad102a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05b9e50729ffc18b8388",
+    "url": "/Posts-app/static/js/main.0c71f5cd.chunk.js"
+  },
+  {
+    "revision": "c9bb7d26f5807511133a",
+    "url": "/Posts-app/static/js/runtime-main.28c7e1e4.js"
+  }
+]);
